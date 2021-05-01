e78903d205eae4b93a4472f9953c194a7d750a68
David Scourfield <daiscog@users.noreply.github.com>
2021-05-01 11:34:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ngx-http-request-status
+
+An Angular library for wrapping Angular HttpClient requests/responses with loading 
+& error information.
+
+----
+
+This project was generated using [Nx](https://nx.dev).
---
+++ apps/examples/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-http-request-status-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'examples';
+}
