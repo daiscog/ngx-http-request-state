8d0ef71acdec7f4d8667d525ad3a27682a9b003f
David Scourfield <daiscog@users.noreply.github.com>
2021-05-01 14:59:29 +0100

    Allow using httpRequestStates operator on any observable, not just HttpResponse

--- libs/http-request-state/src/lib/operators.spec.ts
+++ libs/http-request-state/src/lib/operators.spec.ts
@@ -5,62 +5,81 @@ import { loadingState, loadedState, errorState } from './builders';
 
 describe('operators', () => {
   describe('httpRequestStates', () => {
-    describe('without a mapping function', () => {
-      it('should emit loading, then the response body for a successful request', () => {
-        const response = new HttpResponse({
-          body: {
-            foo: 'bar',
-          },
-        });
-        const source = cold('  --------(b|)', { b: response });
-        const expected = cold('l-------(b|)', {
-          l: loadingState(),
-          b: loadedState(response.body),
+    describe('when source observable emits an HttpResponse', () => {
+      describe('without a mapping function', () => {
+        it('should emit loading, then the response body for a successful request', () => {
+          const response = new HttpResponse({
+            body: {
+              foo: 'bar',
+            },
+          });
+          const source = cold('  --------(b|)', { b: response });
+          const expected = cold('l-------(b|)', {
+            l: loadingState(),
+            b: loadedState(response.body),
+          });
+
+          expect(source.pipe(httpRequestStates())).toBeObservable(expected);
         });
 
-        expect(source.pipe(httpRequestStates())).toBeObservable(expected);
+        it('should emit loading, then the error response for an unsuccessful request', () => {
+          const error = new HttpErrorResponse({});
+          const source = cold('  --------(#|)', {}, error);
+          const expected = cold('l-------(e|)', {
+            l: loadingState(),
+            e: errorState(error),
+          });
+
+          expect(source.pipe(httpRequestStates())).toBeObservable(expected);
+        });
       });
 
-      it('should emit loading, then the error response for an unsuccessful request', () => {
-        const error = new HttpErrorResponse({});
-        const source = cold('  --------(#|)', {}, error);
-        const expected = cold('l-------(e|)', {
-          l: loadingState(),
-          e: errorState(error),
+      describe('with a mapping function', () => {
+        const mapper = (response) => `Hello, ${response.body.foo}`;
+
+        it('should emit loading, then the result of applying the response mapper for a successful request', () => {
+          const response = new HttpResponse({
+            body: {
+              foo: 'World',
+            },
+          });
+
+          const source = cold('  --------(b|)', { b: response });
+          const expected = cold('l-------(b|)', {
+            l: loadingState(),
+            b: loadedState(mapper(response)),
+          });
+
+          expect(source.pipe(httpRequestStates(mapper))).toBeObservable(
+            expected
+          );
         });
 
-        expect(source.pipe(httpRequestStates())).toBeObservable(expected);
+        it('should emit loading, then the request error for an unsuccessful request', () => {
+          const error = new HttpErrorResponse({});
+          const source = cold('  --------(#|)', {}, error);
+          const expected = cold('l-------(e|)', {
+            l: loadingState(),
+            e: errorState(error),
+          });
+
+          expect(source.pipe(httpRequestStates(mapper))).toBeObservable(
+            expected
+          );
+        });
       });
     });
 
-    describe('with a mapping function', () => {
-      const mapper = (response) => `Hello, ${response.body.foo}`;
-
-      it('should emit loading, then the result of applying the response mapper for a successful request', () => {
-        const response = new HttpResponse({
-          body: {
-            foo: 'World',
-          },
-        });
-
-        const source = cold('  --------(b|)', { b: response });
+    describe('when source observable emits something other than an HttpResponse', () => {
+      it('should emit a loading event, then a loaded status wrapping the source data', () => {
+        const data = { answer: 42 };
+        const source = cold('  --------(b|)', { b: data });
         const expected = cold('l-------(b|)', {
           l: loadingState(),
-          b: loadedState(mapper(response)),
+          b: loadedState(data),
         });
 
-        expect(source.pipe(httpRequestStates(mapper))).toBeObservable(expected);
-      });
-
-      it('should emit loading, then the request error for an unsuccessful request', () => {
-        const error = new HttpErrorResponse({});
-        const source = cold('  --------(#|)', {}, error);
-        const expected = cold('l-------(e|)', {
-          l: loadingState(),
-          e: errorState(error),
-        });
-
-        expect(source.pipe(httpRequestStates(mapper))).toBeObservable(expected);
+        expect(source.pipe(httpRequestStates())).toBeObservable(expected);
       });
     });
   });
--- libs/http-request-state/src/lib/operators.ts
+++ libs/http-request-state/src/lib/operators.ts
@@ -5,20 +5,26 @@ import { HttpRequestState } from './model';
 import { loadingState, loadedState, errorState } from './builders';
 
 export function httpRequestStates<T>(): (
-  source: Observable<HttpResponse<T>>
+  source: Observable<HttpResponse<T> | T>
 ) => Observable<HttpRequestState<T>>;
 export function httpRequestStates<T, K>(
-  mapResponse: (result: HttpResponse<T>) => K
+  mapResponse: (result: HttpResponse<T> | T) => K
 ): (source: Observable<HttpResponse<T> | T>) => Observable<HttpRequestState<K>>;
 export function httpRequestStates<T, K>(
-  mapResponse?: (result: HttpResponse<T>) => K
+  mapResponse?: (result: HttpResponse<T> | T) => K
 ): (
-  source: Observable<HttpResponse<T>>
+  source: Observable<HttpResponse<T> | T>
 ) => Observable<HttpRequestState<T | K>> {
   return (source: Observable<HttpResponse<T>>) =>
     source.pipe(
       map((result) =>
-        loadedState<T | K>(mapResponse ? mapResponse(result) : result.body)
+        loadedState<T | K>(
+          mapResponse
+            ? mapResponse(result)
+            : result instanceof HttpResponse
+            ? result.body
+            : result
+        )
       ),
       startWith(loadingState<T | K>()),
       catchError((err) => of(errorState<T | K>(err)))
