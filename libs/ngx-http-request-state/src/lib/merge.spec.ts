8931bd2b649941480749a1316b5136d00786cf76
Peter Severin Rasmussen <git@petersr.com>
2023-03-19 09:51:45 +0100

    Added more tests

--- libs/ngx-http-request-state/src/lib/merge.spec.ts
+++ libs/ngx-http-request-state/src/lib/merge.spec.ts
@@ -1,5 +1,5 @@
-import { loadedState, loadingState } from "./builders";
-import { isLoadedState, isLoadingState } from "./type-guards";
+import { errorState, loadedState, loadingState } from "./builders";
+import { isErrorState, isLoadedState, isLoadingState } from "./type-guards";
 import { mergeStates} from "./merge"
 
 const sum = (values: number[]) => values.reduce((a, b) => a + b, 0)
@@ -27,5 +27,36 @@ describe('merge', () => {
 
     expect(isLoadedState(merged)).toBe(true);
     expect(merged.value).toBe(42 + 1337)
+  });
+
+  it('should return error state if one state fails', () => {
+    const loading = loadingState();
+    const loaded = loadedState(42);
+    const error = errorState(new Error("Test"))
+
+    const mergedLoadingError = mergeStates([loading, error], sum);
+
+    expect(isErrorState(mergedLoadingError)).toBe(true);
+    expect(mergedLoadingError.error.message).toBe("Test");
+
+    const mergedLoadedError = mergeStates([loaded, error], sum);
+
+    expect(isErrorState(mergedLoadedError)).toBe(true);
+    expect(mergedLoadedError.error.message).toBe("Test");
+  });
+
+
+  it('should return error state with custom error in case mergeErrors is supplied', () => {
+    const loaded = loadedState(42);
+    const error1 = errorState(new Error("Goodbye"));
+    const error2 = errorState(new Error("world"));
+
+    const mergedLoadingError = mergeStates([loaded, error1, error2], sum, (errors) => {
+      const msg = errors.map(error => error.message).join(" ")
+      return new Error(msg)
+    });
+
+    expect(isErrorState(mergedLoadingError)).toBe(true);
+    expect(mergedLoadingError.error.message).toBe("Goodbye world");
   });
 })
