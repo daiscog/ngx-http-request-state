999b4cf8139d58ce8f77471a277ecefb5ffda5d3
Peter Severin Rasmussen <github@petersr.com>
2023-04-26 10:45:37 +0200

    Introduces a merge function that merges an array of states into a single new state (#11)
    * Added merge function
    
    * Adedd some tests
    
    * Added more tests
    
    * Updated readme
    
    * Update libs/ngx-http-request-state/README.md
    
    Co-authored-by: daiscog <daiscog@users.noreply.github.com>
    
    * npm run format
    
    ---------
    
    Co-authored-by: daiscog <daiscog@users.noreply.github.com>

--- libs/ngx-http-request-state/src/index.ts
+++ libs/ngx-http-request-state/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/model';
 export * from './lib/builders';
 export * from './lib/type-guards';
 export * from './lib/operators';
+export * from './lib/merge';
---
+++ libs/ngx-http-request-state/src/lib/merge.spec.ts
@@ -0,0 +1,65 @@
+import { errorState, loadedState, loadingState } from './builders';
+import { isErrorState, isLoadedState, isLoadingState } from './type-guards';
+import { mergeStates } from './merge';
+
+const sum = (values: number[]) => values.reduce((a, b) => a + b, 0);
+
+describe('merge', () => {
+  it('should return a loadingState if one of the inputs are loading state', () => {
+    const loading = loadingState<number>();
+    const loaded = loadedState(42);
+
+    const mergedLoadingLoading = mergeStates([loading, loading], sum);
+    expect(isLoadingState(mergedLoadingLoading)).toBe(true);
+
+    const mergedLoadingLoaded = mergeStates([loading, loaded], sum);
+    expect(isLoadingState(mergedLoadingLoaded)).toBe(true);
+
+    const mergeLoadedLoading = mergeStates([loaded, loading], sum);
+    expect(isLoadingState(mergeLoadedLoading)).toBe(true);
+  });
+
+  it('should return merged loadedState if all inputs are loaded state', () => {
+    const s1 = loadedState(42);
+    const s2 = loadedState(1337);
+
+    const merged = mergeStates([s1, s2], sum);
+
+    expect(isLoadedState(merged)).toBe(true);
+    expect(merged.value).toBe(42 + 1337);
+  });
+
+  it('should return error state if one state fails', () => {
+    const loading = loadingState();
+    const loaded = loadedState(42);
+    const error = errorState(new Error('Test'));
+
+    const mergedLoadingError = mergeStates([loading, error], sum);
+
+    expect(isErrorState(mergedLoadingError)).toBe(true);
+    expect(mergedLoadingError.error.message).toBe('Test');
+
+    const mergedLoadedError = mergeStates([loaded, error], sum);
+
+    expect(isErrorState(mergedLoadedError)).toBe(true);
+    expect(mergedLoadedError.error.message).toBe('Test');
+  });
+
+  it('should return error state with custom error in case mergeErrors is supplied', () => {
+    const loaded = loadedState(42);
+    const error1 = errorState(new Error('Goodbye'));
+    const error2 = errorState(new Error('world'));
+
+    const mergedLoadingError = mergeStates(
+      [loaded, error1, error2],
+      sum,
+      (errors) => {
+        const msg = errors.map((error) => error.message).join(' ');
+        return new Error(msg);
+      }
+    );
+
+    expect(isErrorState(mergedLoadingError)).toBe(true);
+    expect(mergedLoadingError.error.message).toBe('Goodbye world');
+  });
+});
