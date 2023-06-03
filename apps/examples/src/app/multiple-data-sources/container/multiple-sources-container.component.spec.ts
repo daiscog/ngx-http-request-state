aafe241dbdcd6f8f8930eb18d989016d66248a0e
David Scourfield <daiscog@users.noreply.github.com>
2023-06-04 00:16:28 +0100

    Other multi-source unit tests

--- apps/examples/src/app/multiple-data-sources/container/multiple-sources-container.component.spec.ts
+++ apps/examples/src/app/multiple-data-sources/container/multiple-sources-container.component.spec.ts
@@ -5,10 +5,12 @@ import { Beer, Brews } from '../model/brews';
 import { cold } from 'jest-marbles';
 import { BrewDateForm } from '../model/form';
 import {
+  errorState,
   HttpRequestState,
   loadedState,
   loadingState,
 } from 'ngx-http-request-state';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('MultipleSourcesContainerComponent', () => {
   function setup(): {
@@ -31,9 +33,9 @@ describe('MultipleSourcesContainerComponent', () => {
 
   describe('#beers$', () => {
     it('should emit search results after both streams have loaded', () => {
+      const searches = '    --a----------b----';
       const beforeTiming = '  --(d|)';
       const afterTiming = '   ----(d|)';
-      const searches = '    --a----------b----';
       const expected = '    --L---a------L---b';
 
       const before: Beer[] = [
@@ -76,13 +78,93 @@ describe('MultipleSourcesContainerComponent', () => {
     });
 
     it('should continue to emit for new searches after an earlier error', () => {
+      const searches = '    --a----------a----';
+      const errorTiming = '   ------#';
+      const beforeTiming = '             --(d|)';
+      const afterTiming = '              ----(d|)';
+      const expected = '    --L-----e----L---b';
+
+      const before: Beer[] = [
+        { id: 'a', name: 'Duff', description: 'Duff beer' },
+      ];
+      const after: Beer[] = [
+        { id: 'b', name: 'Stuff', description: 'Stuff beer' },
+      ];
+
+      const searchValues: Record<'a', BrewDateForm> = {
+        a: { year: '1985', month: '02' },
+      };
+
+      const error = new HttpErrorResponse({ status: 503 });
+
+      const expectedValues: Record<
+        string,
+        HttpRequestState<Brews & BrewDateForm>
+      > = {
+        L: loadingState(),
+        e: errorState(error),
+        b: loadedState({
+          before,
+          after,
+          ...searchValues.a,
+        }),
+      };
+
       const { component, service } = setup();
-      //TODO
+
+      service.brewedBefore.mockReturnValue(cold(beforeTiming, { d: before }));
+      service.brewedAfter
+        .mockReturnValueOnce(cold(errorTiming, {}, error))
+        .mockReturnValue(cold(afterTiming, { d: after }));
+
+      cold(searches, searchValues).subscribe((s) => component.search(s));
+
+      expect(component.beers$).toBeObservable(cold(expected, expectedValues));
     });
 
     it('should drop inflight requests when a new search has been triggered', () => {
+      const searches = '    --a----b----';
+      const beforeTiming = '  ---(d|)';
+      const afterTiming = '   ------(d|)';
+      const expected = '    --L----L-----b';
+
+      const before: Beer[] = [
+        { id: 'a', name: 'Duff', description: 'Duff beer' },
+      ];
+      const after: Beer[] = [
+        { id: 'b', name: 'Stuff', description: 'Stuff beer' },
+      ];
+
+      const searchValues: Record<'a' | 'b', BrewDateForm> = {
+        a: { year: '1985', month: '02' },
+        b: { year: '2000', month: '10' },
+      };
+
+      const expectedValues: Record<
+        string,
+        HttpRequestState<Brews & BrewDateForm>
+      > = {
+        L: loadingState(),
+        a: loadedState({
+          before,
+          after,
+          ...searchValues.a,
+        }),
+        b: loadedState({
+          before,
+          after,
+          ...searchValues.b,
+        }),
+      };
+
       const { component, service } = setup();
-      //TODO
+
+      service.brewedBefore.mockReturnValue(cold(beforeTiming, { d: before }));
+      service.brewedAfter.mockReturnValue(cold(afterTiming, { d: after }));
+
+      cold(searches, searchValues).subscribe((s) => component.search(s));
+
+      expect(component.beers$).toBeObservable(cold(expected, expectedValues));
     });
   });
 });
